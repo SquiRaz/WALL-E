6bc04a514327994b780ef363bc5b9fa93f392ee0
SquiRaz <43420956+SquiRaz@users.noreply.github.com>
2018-09-19 21:19:11 +0200

    Add files via upload

---
+++ Créer un bot Discord/commands/play.js
@@ -0,0 +1,30 @@
+const Command = require('./command')
+const YoutubeStream = require('ytdl-core')
+module.exports = class Play extends Command {
+
+  static match (message) {
+    return message.content.startsWith('!play')
+  }
+
+  static action (message) {
+    let voiceChannel = message.guild.channels
+      .filter(function (channel) { return channel.type === 'voice' })
+      .first()
+    let args = message.content.split(' ')
+    voiceChannel
+      .join()
+      .then(function (connection) {
+        let stream = YoutubeStream(args[1])
+        stream.on('error', function () {
+          message.reply("Je n'ai pas réussi à lire cette vidéo :(")
+          connection.disconnect()
+        })
+        connection
+          .playStream(stream)
+          .on('end', function () {
+            connection.disconnect()
+          })
+      })
+  }
+
+}
